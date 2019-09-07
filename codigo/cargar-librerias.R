94b299ad903d81dcb6dd8178738f16d9fe1126eb
pachamaltese <mvargas@dcc.uchile.cl>
2019-09-07 17:36:03 -0400

    falta bibliografia

--- codigo/cargar-librerias.R
+++
@@ -1,9 +0,0 @@
-load <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-} 
-
-packages <- c("psych","GPArotation","lattice","gridExtra")
-load(packages)
